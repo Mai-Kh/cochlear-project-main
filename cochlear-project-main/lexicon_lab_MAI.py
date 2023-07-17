8ec117336dfbd068aa86035c2d617178e710be58
Mai-Kh <30680133+Mai-Kh@users.noreply.github.com>
2023-07-17 15:00:48 -0500

    Create lexicon_lab_MAI.py

---
+++ cochlear-project-main/lexicon_lab_MAI.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 14:16:33 2023
+
+@author: Mai Al-Khatib
+"""
+
+class similariy:
+    
+    def cosine_similarity(word1, word2, model):
+        return 1
+    
+    def pairwise_similarity(data, model):
+        return 1
+    
+class clusters:
+    
+    def compute_clusters(data, model):
+        return 1
+    
+    def visualize_clusters(ID):
+        return 1
+    
